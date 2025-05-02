21466676a58b27fe9a1c9ef4d07131f01067665a
MikeeOo <michal.olszewskki@gmail.com>
2025-05-02 21:59:06 +0200

    Implement routing and data fetching in App component; add API constants file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import './App.css'
+import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { API } from './constants';
+import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
+	const [data, setData] = useState<{ message: string }>({ message: '' });
 
-  return (
-    <>
-		<div>test</div>
-    </>
-  )
+	useEffect(() => {
+		fetch(API)
+			.then((response) => response.json())
+			.then((data) => {
+				setData(data);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+			});
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<nav>
+					<ul
+						style={{
+							display: 'flex',
+							gap: '1rem',
+							listStyle: 'none',
+							padding: '1rem',
+						}}>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/products">Products</Link>
+						</li>
+					</ul>
+				</nav>
+				<div>{data.message}</div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/products" element={<Products />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
