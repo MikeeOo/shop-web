9551c0a4fbf8e3cae49c8d46f293d22021f7fe43
MikeeOo <michal.olszewskki@gmail.com>
2025-05-03 17:06:38 +0200

    Configure Vite build settings: set base path and output directory for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/dist/',
 	server: {
 		proxy: {
 			'/api': {
 				target: 'http://localhost:8000',
 			},
 		},
+	},
+	build: {
+		outDir: '../shop-api/public/dist',
+		emptyOutDir: true,
 	},
 });
