377a1f11dfd2187f22b726a09e53469ba8e61196
MikeeOo <michal.olszewskki@gmail.com>
2025-05-19 16:56:59 +0200

    Add Tailwind CSS plugin to Vite configuration and adjust base path for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/dist/',
+	plugins: [react(), tailwindcss()],
+	base: isProduction ? '/dist/' : '/',
 	server: {
 		proxy: {
 			'/api': {
