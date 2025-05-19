7fc990bec7d91601ce9080215a3bf01becce33df
MikeeOo <michal.olszewskki@gmail.com>
2025-05-19 16:57:10 +0200

    Refactor App component to use Outlet for routing; remove data fetching and navigation elements for cleaner structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,10 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { API } from './constants';
-import Home from './pages/Home';
-import Products from './pages/Products';
+import { Outlet } from 'react-router-dom';
 
-function App() {
-	const [data, setData] = useState<{ message: string }>({ message: '' });
-
-	useEffect(() => {
-		fetch(API)
-			.then((response) => response.json())
-			.then((data) => {
-				setData(data);
-			})
-			.catch((error) => {
-				console.error('Error fetching data:', error);
-			});
-	}, []);
-
+export default function App() {
 	return (
-		<BrowserRouter>
-			<div className="container">
-				<nav>
-					<ul
-						style={{
-							display: 'flex',
-							gap: '1rem',
-							listStyle: 'none',
-							padding: '1rem',
-						}}>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/products">Products</Link>
-						</li>
-					</ul>
-				</nav>
-				<div>{data.message}</div>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/products" element={<Products />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		// <div className="text-2xl font-semibold">
+		<div className="text-xl bg-black text-white">
+			<Outlet />
+		</div>
 	);
 }
-
-export default App;
