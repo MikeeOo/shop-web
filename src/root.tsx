f652dec21f5baeff1fe7dbf5080c038418fe78c5
MikeeOo <michal.olszewskki@gmail.com>
2025-05-23 16:54:23 +0200

    Refactor root component to update import paths for router and styles, transitioning from Tailwind CSS to custom styles for improved organization.

--- src/root.tsx
+++ src/root.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from './router.tsx';
-import './tailwind.css';
+import { router } from './router';
+import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
