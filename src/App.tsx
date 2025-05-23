2af25348b1b5cdd9f630b708b69268e4f73d7849
MikeeOo <michal.olszewskki@gmail.com>
2025-05-23 16:58:18 +0200

    Update App component styles to enhance typography and layout consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export default function App() {
 	return (
-		// <div className="text-2xl font-semibold">
-		<div className="text-xl bg-black text-white">
+		<div className="text-2xl font-semibold">
+			{/* <div className="bg-black text-xl text-white"> */}
 			<Outlet />
 		</div>
 	);
