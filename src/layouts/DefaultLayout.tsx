3711aed7684e4a41bee83ffbcb061f0ad256dcee
MikeeOo <michal.olszewskki@gmail.com>
2025-05-25 19:32:33 +0200

    Add Button component to DefaultLayout for enhanced interactivity and user engagement.

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
 import NavBar from '../partials/NavBar';
+import { Button } from '@/components/common/atoms/buttons';
 
 export default function DefaultLayout() {
 	return (
 		<div className="h-screen">
 			<div className="border-b-2">DefaultLayout</div>
 			<NavBar />
+			<Button.Primary text="Click me" />
 			<Outlet />
 		</div>
 	);
